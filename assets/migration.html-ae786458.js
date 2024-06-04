c908badadab8bb9cb2bff0e533d1bf01d22dedb6
VuesaxAlphaBot <73821256+tranthinh-coding@users.noreply.github.com>
2024-06-04 05:50:42 +0000

    Deploying to main from @ vuesax-alpha/vuesax-alpha@c130db6638906d1e8397f9a4b667cc01d29fb1e6 🚀

--- assets/migration.html-ae786458.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"key":"v-d0112c92","path":"/guide/migration.html","title":"Migration","lang":"en-US","frontmatter":{},"headers":[],"git":{"createdTime":1716397951000,"updatedTime":1716397951000,"contributors":[{"name":"Haojie Ning","email":"54093230+badspider7@users.noreply.github.com","commits":1}]}}');export{e as data};
