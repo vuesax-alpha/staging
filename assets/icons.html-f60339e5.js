ec950852d48d803591bb4ddb70a823b817aee1b8
VuesaxAlphaBot <54093230+badspider7@users.noreply.github.com>
2024-08-03 07:33:55 +0000

    Deploying to main from @ vuesax-alpha/vuesax-alpha@3c67ce8fed6a3ff05c93560fabdbf9c43c1aa35b 🚀

---
+++ assets/icons.html-f60339e5.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-03f5c2fe","path":"/theme/icons.html","title":"Icons","lang":"en-US","frontmatter":{},"headers":[],"git":{"createdTime":1722670295000,"updatedTime":1722670295000,"contributors":[{"name":"Haojie Ning","email":"54093230+badspider7@users.noreply.github.com","commits":1}]}}');export{e as data};
