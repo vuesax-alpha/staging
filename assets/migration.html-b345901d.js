58fa0fa4e18a79a60e5fb65412ca61ba8ad8f06c
VuesaxAlphaBot <73821256+tranthinh-coding@users.noreply.github.com>
2024-08-21 14:19:09 +0000

    Deploying to main from @ vuesax-alpha/vuesax-alpha@7d451a00487ad1b885c5953590363598b0b96df7 🚀

--- assets/migration.html-b345901d.js
+++
@@ -1,1 +0,0 @@
-const t=JSON.parse('{"key":"v-d0112c92","path":"/guide/migration.html","title":"Migration","lang":"en-US","frontmatter":{},"headers":[],"git":{"createdTime":1724247790000,"updatedTime":1724247790000,"contributors":[{"name":"Think","email":"73821256+tranthinh-coding@users.noreply.github.com","commits":1}]}}');export{t as data};
